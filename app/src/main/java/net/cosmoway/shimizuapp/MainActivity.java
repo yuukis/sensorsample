cf4ca6e61b55585af8266cce7cef75e3dadb805a
Yuuki Shimizu <shimizu@cosmoway.net>
2015-10-16 11:55:11 +0900

    とりあえず作った

---
+++ app/src/main/java/net/cosmoway/shimizuapp/MainActivity.java
@@ -0,0 +1,62 @@
+package net.cosmoway.shimizuapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private TextView mTextView1;
+    private TextView mTextView2;
+    private TextView mTextView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mTextView1 = (TextView) findViewById(R.id.text1);
+        mTextView2 = (TextView) findViewById(R.id.text2);
+        mTextView3 = (TextView) findViewById(R.id.text3);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        mTextView1.setText(String.format("%f", x));
+        mTextView2.setText(String.format("%f", y));
+        mTextView3.setText(String.format("%f", z));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
